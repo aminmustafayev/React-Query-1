f00af34a268a4a3e88410c573df8347d1399c577
aminmustafayev <aminmustafayev778@gmail.com>
2024-05-14 00:54:47 +0400

    add olunanda eror alinir ne etdimse duzelde bilmedim

---
+++ src/classes/Movies.js
@@ -0,0 +1,9 @@
+class Movies {
+    constructor(title,posterImg,genre,releaseYear){
+        this.title=title,
+        this.posterImg=posterImg,
+        this.genre=genre
+        this.releaseYear=releaseYear
+    }
+}
+export default Movies
